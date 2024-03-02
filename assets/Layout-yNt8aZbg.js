304c187600c7d82d34040903bc827833131f2de7
Alexandr <89970757+AladinAlex@users.noreply.github.com>
2024-03-02 20:31:50 +0300

    Add files via upload

---
+++ assets/Layout-yNt8aZbg.js
@@ -0,0 +1,1 @@
+import{_ as o,a as t,o as s,c as n,e as c,d as a}from"./index-Bu2h9yeX.js";const r={components:{},setup(){}},_={class:"content"},p={class:"inner"};function d(i,u,l,m,f,V){const e=t("RouterView");return s(),n("div",_,[c("div",p,[a(e)])])}const h=o(r,[["render",d]]);export{h as default};
